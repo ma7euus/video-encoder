dee65fa5a0d055fb91ba207b8ace11d252307f7c
ma7euus <mateuspastori@outlook.com>
2020-08-17 22:51:51 -0300

    aplicacao e framework

---
+++ application/services/job_worker.go
@@ -0,0 +1,90 @@
+package services
+
+import (
+	"video-encoder/domain"
+	"video-encoder/framework/utils"
+	"github.com/streadway/amqp"
+	"encoding/json"
+	uuid "github.com/satori/go.uuid"
+	"os"
+	"sync"
+	"time"
+)
+
+type JobWorkerResult struct {
+	Job domain.Job
+	Message *amqp.Delivery
+	Error error
+}
+
+var Mutex = &sync.Mutex{} 
+
+func JobWorker(messageChannel chan amqp.Delivery, returnChan chan JobWorkerResult, jobService JobService, job domain.Job, workerID int) {
+	for message := range messageChannel {
+		err := utils.IsJson(string(message.Body))
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		Mutex.Lock()
+		err = json.Unmarshal(message.Body, &jobService.VideoService.Video)
+		jobService.VideoService.Video.ID = uuid.NewV4().String()
+		Mutex.Unlock()
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.Video.Validate()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		Mutex.Lock()
+		err = jobService.VideoService.InsertVideo()
+		Mutex.Unlock()
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		job.Video = jobService.VideoService.Video
+		job.OutputBucketPath = os.Getenv("OUTPUT_BUCKET_NAME")
+		job.ID = uuid.NewV4().String()
+		job.Status = "STARTING"
+		job.CreatedAt = time.Now()
+
+		Mutex.Lock()
+		_, err = jobService.JobRepository.Insert(&job)
+		Mutex.Unlock()
+		
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		jobService.Job = &job
+		err = jobService.Start()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		returnChan <- returnJobResult(job, message, err)
+
+	}
+}
+
+func returnJobResult(job domain.Job, message amqp.Delivery, err error) JobWorkerResult {
+	result := JobWorkerResult{
+		Job:     job,
+		Message: &message,
+		Error:   err,
+	}
+	return result
+}
